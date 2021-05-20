4d6d6da597171273b20d01511a966cb31632eb44
frank2 <frank2@dc949.org>
2021-05-19 23:59:48 -0700

    add notes for the shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    /* connect to host and port
+     * if connect fails, bail
+     * detect operating system
+       * if Windows, cmd.exe
+       * else, bash
+     * redirect stdin to socket
+     * redirect stdout/stderr to socket
+     * launch shell
+     * while running, check if socket is alive
+     * close socket */
 }
